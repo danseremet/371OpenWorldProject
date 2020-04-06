828121ba9257a73452d29734321ca61d1e450ece
Joanna Lin <33985435+jayohanna@users.noreply.github.com>
2020-04-06 19:15:56 -0400

    Refactor fragment shader

--- shaders/basic/fs.glsl
+++ shaders/basic/fs.glsl
@@ -14,18 +14,32 @@ in vec3 Normal;
 in vec3 vertexColor;
 out vec4 FragColor;
 
-void main() {
+vec3 ambient() {
    vec3 ambient = ambientStrength * lightColor;
+   return ambient;
+}
 
-   vec3 norm = normalize(Normal);
-   vec3 lightDir = normalize(lightPos - FragPos);
+vec3 diffuse(vec3 norm, vec3 lightDir) {
    float diff = diffuseStrength * max(dot(norm, lightDir), 0.0);
    vec3 diffuse = diff * lightColor;
+   return diffuse;
+}
 
-   vec3 viewDir = normalize(viewPos - FragPos);
-   vec3 reflectDir = reflect(-lightDir, norm);
+vec3 specular(vec3 viewDir, vec3 reflectDir) {
    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
    vec3 specular = specularStrength * spec * lightColor;
+   return specular;
+}
+
+void main() {
+   vec3 norm = normalize(Normal);
+   vec3 lightDir = normalize(lightPos - FragPos);
+   vec3 viewDir = normalize(viewPos - FragPos);
+   vec3 reflectDir = reflect(-lightDir, norm);
+
+   vec3 ambient = ambient();
+   vec3 diffuse = diffuse(norm, lightDir);
+   vec3 specular = specular(viewDir, reflectDir);
 
    vec3 result = (ambient + diffuse + specular) * vertexColor;
    FragColor = vec4(result, 1.0);
