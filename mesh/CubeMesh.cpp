aea8e00cf6ad17e75fcb64febb0cdfec7835e3e3
Joanna Lin <33985435+jayohanna@users.noreply.github.com>
2020-04-06 18:24:09 -0400

    Removed texture vector from CubeMesh vertices

--- mesh/CubeMesh.cpp
+++ mesh/CubeMesh.cpp
@@ -17,47 +17,47 @@ void CubeMesh::loadVertices() {
     vec3 RED = vec3(1.0f, 0.0f, 0.0f);
     vertices = {
         // BACK
-        {vec3(-0.5f, -0.5f, -0.5f), RED, vec3(0.0f,  0.0f, -1.0f), vec2(0.0f,  0.0f)}, // F
-        {vec3(0.5f,  0.5f, -0.5f), RED, vec3(0.0f,  0.0f, -1.0f), vec2(1.0f,  1.0f)},  // B
-        {vec3(0.5f, -0.5f, -0.5f), RED, vec3(0.0f,  0.0f, -1.0f), vec2(1.0f,  0.0f)},  // E
-        {vec3(0.5f,  0.5f, -0.5f), RED, vec3(0.0f,  0.0f, -1.0f), vec2(1.0f,  1.0f)},  // B
-        {vec3(-0.5f, -0.5f, -0.5f), RED, vec3(0.0f,  0.0f, -1.0f), vec2(0.0f,  0.0f)}, // F
-        {vec3(-0.5f,  0.5f, -0.5f), RED, vec3(0.0f,  0.0f, -1.0f), vec2(0.0f,  1.0f)}, // C
+        {vec3(-0.5f, -0.5f, -0.5f), RED, vec3(0.0f,  0.0f, -1.0f)}, // F
+        {vec3(0.5f,  0.5f, -0.5f), RED, vec3(0.0f,  0.0f, -1.0f)},  // B
+        {vec3(0.5f, -0.5f, -0.5f), RED, vec3(0.0f,  0.0f, -1.0f)},  // E
+        {vec3(0.5f,  0.5f, -0.5f), RED, vec3(0.0f,  0.0f, -1.0f)},  // B
+        {vec3(-0.5f, -0.5f, -0.5f), RED, vec3(0.0f,  0.0f, -1.0f)}, // F
+        {vec3(-0.5f,  0.5f, -0.5f), RED, vec3(0.0f,  0.0f, -1.0f)}, // C
         // FRONT
-        {vec3(-0.5f, -0.5f,  0.5f), RED, vec3(0.0f,  0.0f,  1.0f), vec2(0.0f,  0.0f)}, // G
-        {vec3(0.5f, -0.5f,  0.5f), RED, vec3(0.0f,  0.0f,  1.0f), vec2(1.0f,  0.0f)},  // H
-        {vec3(0.5f,  0.5f,  0.5f), RED, vec3(0.0f,  0.0f,  1.0f), vec2(1.0f,  1.0f)},  // A
-        {vec3(0.5f,  0.5f,  0.5f), RED, vec3(0.0f,  0.0f,  1.0f), vec2(1.0f,  1.0f)},  // A
-        {vec3(-0.5f,  0.5f,  0.5f), RED, vec3(0.0f,  0.0f,  1.0f), vec2(0.0f,  1.0f)}, // D
-        {vec3(-0.5f, -0.5f,  0.5f), RED, vec3(0.0f,  0.0f,  1.0f), vec2(0.0f,  0.0f)}, // G
+        {vec3(-0.5f, -0.5f,  0.5f), RED, vec3(0.0f,  0.0f,  1.0f)}, // G
+        {vec3(0.5f, -0.5f,  0.5f), RED, vec3(0.0f,  0.0f,  1.0f)},  // H
+        {vec3(0.5f,  0.5f,  0.5f), RED, vec3(0.0f,  0.0f,  1.0f)},  // A
+        {vec3(0.5f,  0.5f,  0.5f), RED, vec3(0.0f,  0.0f,  1.0f)},  // A
+        {vec3(-0.5f,  0.5f,  0.5f), RED, vec3(0.0f,  0.0f,  1.0f)}, // D
+        {vec3(-0.5f, -0.5f,  0.5f), RED, vec3(0.0f,  0.0f,  1.0f)}, // G
         // LEFT
-        {vec3(-0.5f,  0.5f,  0.5f), RED, vec3(-1.0f,  0.0f,  0.0f), vec2(1.0f,  0.0f)},// D
-        {vec3(-0.5f,  0.5f, -0.5f), RED, vec3(-1.0f,  0.0f,  0.0f), vec2(1.0f,  1.0f)},// C
-        {vec3(-0.5f, -0.5f, -0.5f), RED, vec3(-1.0f,  0.0f,  0.0f), vec2(0.0f,  1.0f)},// F
-        {vec3(-0.5f, -0.5f, -0.5f), RED, vec3(-1.0f,  0.0f,  0.0f), vec2(0.0f,  1.0f)},// F
-        {vec3(-0.5f, -0.5f,  0.5f), RED, vec3(-1.0f,  0.0f,  0.0f), vec2(0.0f,  0.0f)},// G
-        {vec3(-0.5f,  0.5f,  0.5f), RED, vec3(-1.0f,  0.0f,  0.0f), vec2(1.0f,  0.0f)},// D
+        {vec3(-0.5f,  0.5f,  0.5f), RED, vec3(-1.0f,  0.0f,  0.0f)},// D
+        {vec3(-0.5f,  0.5f, -0.5f), RED, vec3(-1.0f,  0.0f,  0.0f)},// C
+        {vec3(-0.5f, -0.5f, -0.5f), RED, vec3(-1.0f,  0.0f,  0.0f)},// F
+        {vec3(-0.5f, -0.5f, -0.5f), RED, vec3(-1.0f,  0.0f,  0.0f)},// F
+        {vec3(-0.5f, -0.5f,  0.5f), RED, vec3(-1.0f,  0.0f,  0.0f)},// G
+        {vec3(-0.5f,  0.5f,  0.5f), RED, vec3(-1.0f,  0.0f,  0.0f)},// D
         // RIGHT
-        {vec3(0.5f,  0.5f,  0.5f), RED, vec3(1.0f,  0.0f,  0.0f), vec2(1.0f,  0.0f)},  // A
-        {vec3(0.5f, -0.5f, -0.5f), RED, vec3(1.0f,  0.0f,  0.0f), vec2(0.0f,  1.0f)},  // E
-        {vec3(0.5f,  0.5f, -0.5f), RED, vec3(1.0f,  0.0f,  0.0f), vec2(1.0f,  1.0f)},  // B
-        {vec3(0.5f, -0.5f, -0.5f), RED, vec3(1.0f,  0.0f,  0.0f), vec2(0.0f,  1.0f)},  // E
-        {vec3(0.5f,  0.5f,  0.5f), RED, vec3(1.0f,  0.0f,  0.0f), vec2(1.0f,  0.0f)},  // A
-        {vec3(0.5f, -0.5f,  0.5f), RED, vec3(1.0f,  0.0f,  0.0f), vec2(0.0f,  0.0f)},  // H
+        {vec3(0.5f,  0.5f,  0.5f), RED, vec3(1.0f,  0.0f,  0.0f)},  // A
+        {vec3(0.5f, -0.5f, -0.5f), RED, vec3(1.0f,  0.0f,  0.0f)},  // E
+        {vec3(0.5f,  0.5f, -0.5f), RED, vec3(1.0f,  0.0f,  0.0f)},  // B
+        {vec3(0.5f, -0.5f, -0.5f), RED, vec3(1.0f,  0.0f,  0.0f)},  // E
+        {vec3(0.5f,  0.5f,  0.5f), RED, vec3(1.0f,  0.0f,  0.0f)},  // A
+        {vec3(0.5f, -0.5f,  0.5f), RED, vec3(1.0f,  0.0f,  0.0f)},  // H
         // BOTTOM
-        {vec3(-0.5f, -0.5f, -0.5f), RED, vec3(0.0f, -1.0f,  0.0f), vec2(0.0f,  1.0f)}, // F
-        {vec3(0.5f, -0.5f, -0.5f), RED, vec3(0.0f, -1.0f,  0.0f), vec2(1.0f,  1.0f)},  // E
-        {vec3(0.5f, -0.5f,  0.5f), RED, vec3(0.0f, -1.0f,  0.0f), vec2(1.0f,  0.0f)},  // H
-        {vec3(0.5f, -0.5f,  0.5f), RED, vec3(0.0f, -1.0f,  0.0f), vec2(1.0f,  0.0f)},  // H
-        {vec3(-0.5f, -0.5f,  0.5f), RED, vec3(0.0f, -1.0f,  0.0f), vec2(0.0f,  0.0f)}, // G
-        {vec3(-0.5f, -0.5f, -0.5f), RED, vec3(0.0f, -1.0f,  0.0f), vec2(0.0f,  1.0f)}, // F
+        {vec3(-0.5f, -0.5f, -0.5f), RED, vec3(0.0f, -1.0f,  0.0f)}, // F
+        {vec3(0.5f, -0.5f, -0.5f), RED, vec3(0.0f, -1.0f,  0.0f)},  // E
+        {vec3(0.5f, -0.5f,  0.5f), RED, vec3(0.0f, -1.0f,  0.0f)},  // H
+        {vec3(0.5f, -0.5f,  0.5f), RED, vec3(0.0f, -1.0f,  0.0f)},  // H
+        {vec3(-0.5f, -0.5f,  0.5f), RED, vec3(0.0f, -1.0f,  0.0f)}, // G
+        {vec3(-0.5f, -0.5f, -0.5f), RED, vec3(0.0f, -1.0f,  0.0f)}, // F
         // UP
-        {vec3(-0.5f,  0.5f, -0.5f), RED, vec3(0.0f,  1.0f,  0.0f), vec2(0.0f,  1.0f)}, // C
-        {vec3(0.5f,  0.5f,  0.5f), RED, vec3(0.0f,  1.0f,  0.0f), vec2(1.0f,  0.0f)},  // A
-        {vec3(0.5f,  0.5f, -0.5f), RED, vec3(0.0f,  1.0f,  0.0f), vec2(1.0f,  1.0f)},  // B
-        {vec3(0.5f,  0.5f,  0.5f), RED, vec3(0.0f,  1.0f,  0.0f), vec2(1.0f,  0.0f)},  // A
-        {vec3(-0.5f,  0.5f, -0.5f), RED, vec3(0.0f,  1.0f,  0.0f), vec2(0.0f,  1.0f)}, // C
-        {vec3(-0.5f,  0.5f,  0.5f), RED, vec3(0.0f,  1.0f,  0.0f), vec2(0.0f,  0.0f)}  // D
+        {vec3(-0.5f,  0.5f, -0.5f), RED, vec3(0.0f,  1.0f,  0.0f)}, // C
+        {vec3(0.5f,  0.5f,  0.5f), RED, vec3(0.0f,  1.0f,  0.0f)},  // A
+        {vec3(0.5f,  0.5f, -0.5f), RED, vec3(0.0f,  1.0f,  0.0f)},  // B
+        {vec3(0.5f,  0.5f,  0.5f), RED, vec3(0.0f,  1.0f,  0.0f)},  // A
+        {vec3(-0.5f,  0.5f, -0.5f), RED, vec3(0.0f,  1.0f,  0.0f)}, // C
+        {vec3(-0.5f,  0.5f,  0.5f), RED, vec3(0.0f,  1.0f,  0.0f)}  // D
     };
 }
 
